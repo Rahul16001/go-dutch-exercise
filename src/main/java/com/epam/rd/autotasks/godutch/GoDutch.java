410e89df26147d512d4a7f39ade04cded30e123a
Rahul D <rahul_d@epam.com>
2024-07-21 00:49:28 +0530

    My First Commit

--- src/main/java/com/epam/rd/autotasks/godutch/GoDutch.java
+++ src/main/java/com/epam/rd/autotasks/godutch/GoDutch.java
@@ -1,8 +1,26 @@
 package com.epam.rd.autotasks.godutch;
 
+import java.util.Scanner;
+
 public class GoDutch {
 
     public static void main(String[] args) {
         //Write code here
+        int billAmount;
+        int numberOfFriends;
+        Scanner scan = new Scanner(System.in);
+        billAmount = scan.nextInt();
+        numberOfFriends = scan.nextInt();
+
+
+        if(billAmount < 0) System.out.println("Bill total amount cannot be negative");
+        else if(numberOfFriends <= 0) System.out.println("Number of friends cannot be negative or zero");
+        else {
+            int total = (int)(1.1 * billAmount);
+            int moneyDivide = total / numberOfFriends;
+            System.out.println(moneyDivide);
+        }
+
+
     }
 }
